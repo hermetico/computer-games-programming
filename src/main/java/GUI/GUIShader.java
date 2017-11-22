b4441a3a8b2ff34332cd3274ca3661feb6c9284b
hermetico <yojmarinv@gmail.com>
2017-11-22 13:44:41 +0100

    rendering guis

---
+++ src/main/java/GUI/GUIShader.java
@@ -0,0 +1,29 @@
+package GUI;
+
+import org.joml.Matrix4f;
+import shaders.ShaderProgram;
+
+public class GUIShader extends ShaderProgram {
+    private static final String VERTEX_FILE = "/shaders/GUIVertexShader.glsl";
+    private static final String FRAGMENT_FILE = "/shaders/GUIFragmentShader.glsl";
+
+    private int location_transformationMatrix;
+
+    public GUIShader() {
+        super(VERTEX_FILE, FRAGMENT_FILE);
+    }
+
+    public void loadTransformation(Matrix4f matrix){
+        super.loadMatrix(location_transformationMatrix, matrix);
+    }
+
+    @Override
+    protected void getAllUniformLocation() {
+        location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+    }
+
+    @Override
+    protected void bindAttributes() {
+        super.bindAttribute(0, "position");
+    }
+}
