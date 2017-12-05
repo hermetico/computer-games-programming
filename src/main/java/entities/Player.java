7849a962ea34ff5082cfbf4c053a0f185fef942e
malte <m_stei44@uni-muenster.de>
2017-12-05 15:17:58 +0100

    changes for collision detection

--- src/main/java/entities/Player.java
+++ src/main/java/entities/Player.java
@@ -4,45 +4,64 @@ import models.TexturedModel;
 import org.joml.Vector3f;
 import terrains.Terrain;
 import utils.KeyboardInput;
+import collision.AABB;
+
+
+import java.util.List;
 
 import static org.lwjgl.glfw.GLFW.*;
 
 public class Player extends Entity {
 
     private KeyboardInput keyboardInput;
-    private static final float RUN_SPEED = 50;
-    private static final float TURN_SPEED  = 160;
+    private static final float RUN_SPEED = 25;
+    private static final float TURN_SPEED  = 50;
     private static final float yOffset = -90; // model y rotation offset
     private static float GRAVITY = -95;
-    private static final float JUMP_POWER = 55;
+    private static final float JUMP_POWER = 45;
 
-    private float currentSpeed = 0;
-    private float currentTurnSpeed = 0;
+    private float currentSpeed = 10;
+    private float currentTurnSpeed = 10;
     private float upwardsSpeed = 0;
     private boolean alreadyJumping = false;
+
+
 
     public Player(TexturedModel model, Vector3f position, float rotX, float rotY, float rotZ, float scale) {
         super(model, position, rotX, rotY, rotZ, scale);
         keyboardInput = KeyboardInput.getInstance();
     }
 
-    public void update ( float interval, Terrain terrain){
+    public void update (float interval, Terrain terrain, List<Entity> solids) {
         super.increaseRotation(0, currentTurnSpeed * interval, 0);
         float distance = currentSpeed * interval;
         float dx = (float) (distance * Math.sin(Math.toRadians(super.getRotY() + yOffset)));
-        float dz =(float) (distance * Math.cos(Math.toRadians(super.getRotY() + yOffset)));
-        super.increasePosition(dx, 0, dz);
-        upwardsSpeed += GRAVITY * interval;
-        super.increasePosition(0, upwardsSpeed*interval, 0);
-        float terrainHeight = terrain.getTerrainHeight(super.getPosition().x, super.getPosition().z);
-        if (super.getPosition().y < terrainHeight){
-            upwardsSpeed = 0;
-            alreadyJumping = false;
-            super.getPosition().y = terrainHeight;
+        float dz = (float) (distance * Math.cos(Math.toRadians(super.getRotY() + yOffset)));
+
+
+
+            if (!collision(solids)) {
+                super.increasePosition(dx, 0, dz);
+                upwardsSpeed += GRAVITY * interval;
+                super.increasePosition(0, upwardsSpeed * interval, 0);
+
+
+            } else {
+                upwardsSpeed += GRAVITY * interval;
+                super.increasePosition(-dx, 0, -dz);
+            }
+
+            float terrainHeight = terrain.getTerrainHeight(super.getPosition().x, super.getPosition().z);
+
+
+            if (super.getPosition().y < terrainHeight) {
+                upwardsSpeed = 0;
+                alreadyJumping = false;
+                super.getPosition().y = terrainHeight;
+            }
+
         }
 
-
-    }
     public void input(){
         checkInputs();
     }
@@ -53,6 +72,7 @@ public class Player extends Entity {
             alreadyJumping = true;
         }
     }
+
     private void checkInputs(){
         if (keyboardInput.isKeyPressed(GLFW_KEY_W)) {
             this.currentSpeed = RUN_SPEED;
@@ -77,5 +97,15 @@ public class Player extends Entity {
     }
     public float getyOffset(){
         return yOffset;
+    }
+
+    private boolean collision(List<Entity> solids) {
+        boolean collision = false;
+        for (Entity temp : solids) {
+             if (this.bounds.collides(temp.bounds)){
+                 collision = true;
+             }
+        }
+        return collision;
     }
 }
